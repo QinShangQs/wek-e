0a9625022a97f6d57de332e3a2951a15a575a638
lizq <haorunhu@qq.com>
2016-03-11 18:21:52 +0800

    xxx

---
+++ WeiKe/WeiKe.Model/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeiKe.Model
+{
+    public class Users
+    {
+        private int id { get; set; }
+
+    }
+}
